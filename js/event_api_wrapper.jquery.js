e7877f33b1447d8892b8dc755861da12b04e0b11
maasa <maasa.giss2@gmail.com>
2014-06-05 09:17:37 +0900

    retab

--- js/event_api_wrapper.jquery.js
+++ js/event_api_wrapper.jquery.js
@@ -5,9 +5,9 @@
 /*
  * event_api_wrapper.get_events(ids, callback, ym);
  * 
- * @param object	ids: {atnd:'xxxxx', connpass:'xxxxx', zusaar:'xxxxx'}
- * @param function	callback: function(events){ ... }
- * @param array		ym(option): ['201402','201403','201404']
+ * @param object  ids: {atnd:'xxxxx', connpass:'xxxxx', zusaar:'xxxxx'}
+ * @param function  callback: function(events){ ... }
+ * @param array   ym(option): ['201402','201403','201404']
  * 
  * サービス名がキー、IDが値の配列を引数で渡すと
  * イベントをまとめてjson形式で取得し、events配列をcallbackに渡します。
@@ -16,96 +16,96 @@
 
 var event_api_wrapper = {
 
-	common_param: {
-		count: 100
-	},
+  common_param: {
+    count: 100
+  },
 
-	service_setting: {
+  service_setting: {
 
-		atnd: {
-			id_key: 'user_id',
-			url: 'http://api.atnd.org/events/',
-			other_param: {
-				format: 'jsonp'
-			}
-		},
+    atnd: {
+      id_key: 'user_id',
+      url: 'http://api.atnd.org/events/',
+      other_param: {
+        format: 'jsonp'
+      }
+    },
 
-		connpass: {
-			id_key: 'nickname',
-			url: 'http://connpass.com/api/v1/event/',
-			other_param: {
-				format: 'json'
-			}
-		},
+    connpass: {
+      id_key: 'nickname',
+      url: 'http://connpass.com/api/v1/event/',
+      other_param: {
+        format: 'json'
+      }
+    },
 
-		zusaar: {
-			id_key: 'user_id',
-			url: 'http://www.zusaar.com/api/event/',
-			other_param: {
-				format: 'jsonp'
-			}
-		},
-	},
+    zusaar: {
+      id_key: 'user_id',
+      url: 'http://www.zusaar.com/api/event/',
+      other_param: {
+        format: 'jsonp'
+      }
+    },
+  },
 
 
-	get_events: function(ids, callback, ym){
+  get_events: function(ids, callback, ym){
 
-		ids = ids || {};
-		if(ym.length) this.common_param.ym = ym.join(',');
+    ids = ids || {};
+    if(ym.length) this.common_param.ym = ym.join(',');
 
-		var ajaxlist = [];
+    var ajaxlist = [];
 
-		for(service in ids){
-			if(ids[service] && this.service_setting[service]){
-				var data = $.extend({}, this.common_param);
-				if(this.service_setting[service].other_param){
-					data = $.extend(data, this.service_setting[service].other_param);
-				}
-				data[this.service_setting[service].id_key] = ids[service];
+    for(service in ids){
+      if(ids[service] && this.service_setting[service]){
+        var data = $.extend({}, this.common_param);
+        if(this.service_setting[service].other_param){
+          data = $.extend(data, this.service_setting[service].other_param);
+        }
+        data[this.service_setting[service].id_key] = ids[service];
 
-				var add_class = (function(service){
-					return function(data){
-						events = data.events || data.event
-						for(i=events.length-1;i>=0;i--){
-							events[i].service = service;
-						}
-					}
-				})(service);
+        var add_class = (function(service){
+          return function(data){
+            events = data.events || data.event
+            for(i=events.length-1;i>=0;i--){
+              events[i].service = service;
+            }
+          }
+        })(service);
 
-				ajaxlist.push($.ajax({
-					url: this.service_setting[service].url,
-					data: decodeURIComponent($.param(data)),
-					type: 'GET',
-					dataType: 'jsonp',
-					success: add_class
-				}));
-			}
-		}
+        ajaxlist.push($.ajax({
+          url: this.service_setting[service].url,
+          data: decodeURIComponent($.param(data)),
+          type: 'GET',
+          dataType: 'jsonp',
+          success: add_class
+        }));
+      }
+    }
 
-		$.when.apply($, ajaxlist)
-		.done(function () {
+    $.when.apply($, ajaxlist)
+    .done(function () {
 
-			var events = [];
+      var events = [];
 
-			if(ajaxlist.length === 1){
-				events = arguments[0].events || arguments[0].event;
+      if(ajaxlist.length === 1){
+        events = arguments[0].events || arguments[0].event;
 
-			}else{
-				for (var i = 0; i < arguments.length; i++) {
-					events = events.concat( 
-						arguments[i][0].events || arguments[i][0].event
-					);
-				}
-			}
+      }else{
+        for (var i = 0; i < arguments.length; i++) {
+          events = events.concat( 
+            arguments[i][0].events || arguments[i][0].event
+          );
+        }
+      }
 
-			callback(events);
+      callback(events);
 
-		}).fail(function(){
+    }).fail(function(){
 
-			var error = new Error();
-			error.message = "ajax failed";
-			throw error;
-		});
-	}
+      var error = new Error();
+      error.message = "ajax failed";
+      throw error;
+    });
+  }
 
 };
--- js/script.js
+++ js/script.js
@@ -1,268 +1,268 @@
 $(function(){
 
-	var get_events_from_input = function(callback, ym){
+  var get_events_from_input = function(callback, ym){
 
-		var ids = {};
-		ym  = ym || [];
+    var ids = {};
+    ym  = ym || [];
 
-		$('#first .item :text').each(function() {
-			var input_id = $(this).attr('id');
-			ids[input_id] = $(this).val() || localStorage[input_id] || $.cookie(input_id);
-		});
+    $('#first .item :text').each(function() {
+      var input_id = $(this).attr('id');
+      ids[input_id] = $(this).val() || localStorage[input_id] || $.cookie(input_id);
+    });
 
-		// 帰ってきたデータをfullcalendar.jsが受け取れる形にフォーマット
-		var format_events = function(events){
-			var formatted = [];
-			var events_length = events.length;
-			for(var i = 0; i < events.length; i++){
-				var e = events[i];
-				var add_gcal_url = 'http://www.google.com/calendar/event?' +
-					'action='	+ 'TEMPLATE' +
-					'&text='	+ encodeURIComponent(e.title)   +
-					'&details='	+ encodeURIComponent(e.event_url) +
-					'&location='+ encodeURIComponent(e.address)    +
-					'&dates='	+ getUTC(e.started_at)+'/'+getUTC(e.ended_at) +
-					'&trp='		+ 'false' +
-					'&sprop='	+ encodeURIComponent('http://yoshiko-pg.github.io/oreore_cal/') +
-					'&sprop='	+ 'name:oreore_cal';
+    // 帰ってきたデータをfullcalendar.jsが受け取れる形にフォーマット
+    var format_events = function(events){
+      var formatted = [];
+      var events_length = events.length;
+      for(var i = 0; i < events.length; i++){
+        var e = events[i];
+        var add_gcal_url = 'http://www.google.com/calendar/event?' +
+          'action=' + 'TEMPLATE' +
+          '&text='  + encodeURIComponent(e.title)   +
+          '&details=' + encodeURIComponent(e.event_url) +
+          '&location='+ encodeURIComponent(e.address)    +
+          '&dates=' + getUTC(e.started_at)+'/'+getUTC(e.ended_at) +
+          '&trp='   + 'false' +
+          '&sprop=' + encodeURIComponent('http://yoshiko-pg.github.io/oreore_cal/') +
+          '&sprop=' + 'name:oreore_cal';
 
-				formatted.push({
-					title: e.title,
-					start: e.started_at,
-					end: e.ended_at,
-					allDay: false,
-					popup: '<div class="modal fade" id="event_popup" tabindex="-1" role="dialog" aria-hidden="true">' + 
-							'<div class="modal-dialog modal-lg">' + 
-							'<div class="modal-content">' +
-							'<div class="modal-header">' +
-								'<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
-								'<h4 class="modal-title">'+e.title+'</h4>' +
-							'</div>' +
-							'<div class="modal-body">' + 
-								'<p>開始：' + popup_date(e.started_at) + '</p>' + 
-								'<p>終了：' + popup_date(e.ended_at) + '</p>' + 
-								'<p>場所：<a href="https://maps.google.com/maps?q='+e.address+'" target="_blank">' + e.address + ' ' + e.place + ' <i class="fa fa-external-link"></i></a></p>' +
-								'<p>' + e.catch + '</p>' + 
-								'<p><a href="#" class="desc_toggle"><i class="fa fa-plus-square-o"></i> 詳細を表示</a></p>' + 
-								'<div class="desc">' + marked(e.description) + '</div>' + 
-						   '</div>' +
-						   '<div class="modal-footer">' + 
-								'<a href="'+add_gcal_url+'" target="_blank" class="btn btn-success">Add to Google calendar <i class="fa fa-calendar"></i></a>' +
-								'<a href="'+e.event_url+'" target="_blank" class="btn btn-primary">Open event page <i class="fa fa-external-link"></i></a>' +
-								'<button type="button" class="btn btn-default" data-dismiss="modal">Close</button> ' +
-							'</div>' + 
-							'</div>' + 
-							'</div>' + 
-						'</div>',
-					className: e.service
-				});
-			}
+        formatted.push({
+          title: e.title,
+          start: e.started_at,
+          end: e.ended_at,
+          allDay: false,
+          popup: '<div class="modal fade" id="event_popup" tabindex="-1" role="dialog" aria-hidden="true">' + 
+              '<div class="modal-dialog modal-lg">' + 
+              '<div class="modal-content">' +
+              '<div class="modal-header">' +
+                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
+                '<h4 class="modal-title">'+e.title+'</h4>' +
+              '</div>' +
+              '<div class="modal-body">' + 
+                '<p>開始：' + popup_date(e.started_at) + '</p>' + 
+                '<p>終了：' + popup_date(e.ended_at) + '</p>' + 
+                '<p>場所：<a href="https://maps.google.com/maps?q='+e.address+'" target="_blank">' + e.address + ' ' + e.place + ' <i class="fa fa-external-link"></i></a></p>' +
+                '<p>' + e.catch + '</p>' + 
+                '<p><a href="#" class="desc_toggle"><i class="fa fa-plus-square-o"></i> 詳細を表示</a></p>' + 
+                '<div class="desc">' + marked(e.description) + '</div>' + 
+               '</div>' +
+               '<div class="modal-footer">' + 
+                '<a href="'+add_gcal_url+'" target="_blank" class="btn btn-success">Add to Google calendar <i class="fa fa-calendar"></i></a>' +
+                '<a href="'+e.event_url+'" target="_blank" class="btn btn-primary">Open event page <i class="fa fa-external-link"></i></a>' +
+                '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button> ' +
+              '</div>' + 
+              '</div>' + 
+              '</div>' + 
+            '</div>',
+          className: e.service
+        });
+      }
 
-			return formatted;
-		}
-		
-		try{
-			event_api_wrapper.get_events(ids, function(events){
-				callback(format_events(events));
-			}, ym);
+      return formatted;
+    }
+    
+    try{
+      event_api_wrapper.get_events(ids, function(events){
+        callback(format_events(events));
+      }, ym);
 
-		}catch(e){
-			console.log('Error: '+e.message);
-			$('#get_event_result').text('Sorry, fetch events error');
-			$('#loading').hide();
-		}	
-	};
+    }catch(e){
+      console.log('Error: '+e.message);
+      $('#get_event_result').text('Sorry, fetch events error');
+      $('#loading').hide();
+    } 
+  };
 
-	var format_ym = function(start, end){
-		var i = new Date(start.getFullYear(), start.getMonth(), 1, 0, 0, 0);
-		var end_time = new Date(end.getFullYear(), end.getMonth(), 1, 0, 0, 0).getTime();
-		var ym_format = function(d){ 
-			return d.getFullYear()+((('0'+(d.getMonth()+1)).slice(-2)));
-		};
-		var ym_array = [];
+  var format_ym = function(start, end){
+    var i = new Date(start.getFullYear(), start.getMonth(), 1, 0, 0, 0);
+    var end_time = new Date(end.getFullYear(), end.getMonth(), 1, 0, 0, 0).getTime();
+    var ym_format = function(d){ 
+      return d.getFullYear()+((('0'+(d.getMonth()+1)).slice(-2)));
+    };
+    var ym_array = [];
 
-		for(; i.getTime() <= end_time; i = new Date(i.getFullYear(), i.getMonth()+1, 1, 0, 0, 0)){ 
-			ym_array.push(ym_format(i));
-		}
-		return ym_array;
-	};
+    for(; i.getTime() <= end_time; i = new Date(i.getFullYear(), i.getMonth()+1, 1, 0, 0, 0)){ 
+      ym_array.push(ym_format(i));
+    }
+    return ym_array;
+  };
 
-	var zerofill = function(num){
-		return ('0'+num).slice(-2);
-	}
+  var zerofill = function(num){
+    return ('0'+num).slice(-2);
+  }
 
-	var getUTC = function(date_str){
-		var date = new Date(date_str);
-		return date.getUTCFullYear() +
-			zerofill(date.getUTCMonth()+1) +
-			zerofill(date.getUTCDate()) +
-			'T' +
-			zerofill(date.getUTCHours()) +
-			zerofill(date.getUTCMinutes()) +
-			zerofill(date.getUTCSeconds()) +
-			'Z';
-	};
+  var getUTC = function(date_str){
+    var date = new Date(date_str);
+    return date.getUTCFullYear() +
+      zerofill(date.getUTCMonth()+1) +
+      zerofill(date.getUTCDate()) +
+      'T' +
+      zerofill(date.getUTCHours()) +
+      zerofill(date.getUTCMinutes()) +
+      zerofill(date.getUTCSeconds()) +
+      'Z';
+  };
 
-	var cal_option = {
-		header: {
-			left: 'month,agendaWeek,agendaDay',
-			center: 'title',
-			right: 'prev,next today'
-		},
-		titleFormat: {
-			month: 'yyyy - MM',
-			week: "[yyyy] M/d{ '-' [yyyy ]M/d}",
-			day: 'yyyy M/d, dddd'
-		},
-		columnFormat: {
-			month: 'ddd',
-			week: 'M/d ddd',
-			day: 'M/d dddd'
-		},
-		axisFormat: 'H:mm',
-		timeFormat: {
-			'': 'H:mm', // default
-			agenda: 'H:mm{ - H:mm}'
-		},	
-		firstDay: 1,
-		firstHour: 9,
-		buttonText: {
-			prev: "<i class='fa fa-chevron-left'></i>",
-			next: "<i class='fa fa-chevron-right'></i>",
-			prevYear: "<i class='fa fa-chevron-left'></i>",
-			nextYear: "<i class='fa fa-chevron-right'></i>",
-			today: 'today',
-			month: 'month',
-			week: 'week',
-			day: 'day'
-		},
-		editable: false,
-		events: function(start, end, callback) {
-			get_events_from_input(function(events){
-				callback(events);
-			}, format_ym(start, end));
-		},
-		eventClick: function(calEvent, jsEvent) {
-			if($('#event_popup').size()){
-				$('#event_popup').remove();
-			}
-			$('body').prepend(calEvent.popup);
-			$('#event_popup').modal('toggle');
-			$('.desc_toggle').on('click', function(){
-				$('.desc').toggleClass('disp');
-				$('.desc_toggle i')
-				.toggleClass('fa-plus-square-o')
-				.toggleClass('fa-minus-square-o');
-				return false;
-			});
-		},
-		dayClick: function(date, allDay, jsEvent, view) {
-			if (view.name !== 'agendaDay') {
-				$('#calendar').fullCalendar('changeView', 'agendaDay').fullCalendar('gotoDate', date.getFullYear(), date.getMonth(), date.getDate());
-			}
-		},
-		loading: function(bool) {
-			if (bool) $('#loading').show();
-			else $('#loading').hide();
-		}
-	};
+  var cal_option = {
+    header: {
+      left: 'month,agendaWeek,agendaDay',
+      center: 'title',
+      right: 'prev,next today'
+    },
+    titleFormat: {
+      month: 'yyyy - MM',
+      week: "[yyyy] M/d{ '-' [yyyy ]M/d}",
+      day: 'yyyy M/d, dddd'
+    },
+    columnFormat: {
+      month: 'ddd',
+      week: 'M/d ddd',
+      day: 'M/d dddd'
+    },
+    axisFormat: 'H:mm',
+    timeFormat: {
+      '': 'H:mm', // default
+      agenda: 'H:mm{ - H:mm}'
+    },  
+    firstDay: 1,
+    firstHour: 9,
+    buttonText: {
+      prev: "<i class='fa fa-chevron-left'></i>",
+      next: "<i class='fa fa-chevron-right'></i>",
+      prevYear: "<i class='fa fa-chevron-left'></i>",
+      nextYear: "<i class='fa fa-chevron-right'></i>",
+      today: 'today',
+      month: 'month',
+      week: 'week',
+      day: 'day'
+    },
+    editable: false,
+    events: function(start, end, callback) {
+      get_events_from_input(function(events){
+        callback(events);
+      }, format_ym(start, end));
+    },
+    eventClick: function(calEvent, jsEvent) {
+      if($('#event_popup').size()){
+        $('#event_popup').remove();
+      }
+      $('body').prepend(calEvent.popup);
+      $('#event_popup').modal('toggle');
+      $('.desc_toggle').on('click', function(){
+        $('.desc').toggleClass('disp');
+        $('.desc_toggle i')
+        .toggleClass('fa-plus-square-o')
+        .toggleClass('fa-minus-square-o');
+        return false;
+      });
+    },
+    dayClick: function(date, allDay, jsEvent, view) {
+      if (view.name !== 'agendaDay') {
+        $('#calendar').fullCalendar('changeView', 'agendaDay').fullCalendar('gotoDate', date.getFullYear(), date.getMonth(), date.getDate());
+      }
+    },
+    loading: function(bool) {
+      if (bool) $('#loading').show();
+      else $('#loading').hide();
+    }
+  };
 
-	// カレンダーの表示
-	$('#calendar').fullCalendar(cal_option);
+  // カレンダーの表示
+  $('#calendar').fullCalendar(cal_option);
 
-	// 見た目調整（集合ボタンをbtn-groupで囲いたい）
-	$("#calendar .fc-header-left, #calendar .fc-header-right").each(function(){
-		$(this).wrapInner('<div class="btn-group"></div>');
-		var flag = false;
-		$(this).find('.btn-group').children('span').each(function(){
-			if(flag){
-				$(this).parents('td').append($(this));
-			}else{
-				flag = $(this).hasClass('fc-corner-right') ? true : false;
-			}
-		});
-	});
+  // 見た目調整（集合ボタンをbtn-groupで囲いたい）
+  $("#calendar .fc-header-left, #calendar .fc-header-right").each(function(){
+    $(this).wrapInner('<div class="btn-group"></div>');
+    var flag = false;
+    $(this).find('.btn-group').children('span').each(function(){
+      if(flag){
+        $(this).parents('td').append($(this));
+      }else{
+        flag = $(this).hasClass('fc-corner-right') ? true : false;
+      }
+    });
+  });
 
-	// ID入力エリアの表示非表示
-	$('#tab').on('click', function(){
-		var header = $('#header');
-		var margin = (header.css('margin-top') === '0px' ? 0-header.height()+5 : 0);
-		header.animate({marginTop: margin}, 300);
-	});
+  // ID入力エリアの表示非表示
+  $('#tab').on('click', function(){
+    var header = $('#header');
+    var margin = (header.css('margin-top') === '0px' ? 0-header.height()+5 : 0);
+    header.animate({marginTop: margin}, 300);
+  });
 
-	// ID場所ヘルプの表示
-	$('#where_id_link').on('click', function(){
-		$('#where_id').toggle();
-		return false;
-	});
+  // ID場所ヘルプの表示
+  $('#where_id_link').on('click', function(){
+    $('#where_id').toggle();
+    return false;
+  });
 
-	// イベント表示バインド
-	$('#view').click(function() {
-		$('#calendar').fullCalendar('refetchEvents');
-	});
+  // イベント表示バインド
+  $('#view').click(function() {
+    $('#calendar').fullCalendar('refetchEvents');
+  });
 
-	// イベント表示
-	$('#view').click();
-	
-	// IDの保存/削除
-	var ls_btn_toggle = function(flag, unedit){
+  // イベント表示
+  $('#view').click();
+  
+  // IDの保存/削除
+  var ls_btn_toggle = function(flag, unedit){
 
-		var notEmpty = function(){ return this.value.length !== 0; }
+    var notEmpty = function(){ return this.value.length !== 0; }
 
-		// localStorageをセット
-		if(flag){ 
-			var ls_edit = function(){
-				localStorage[$(this).attr('id')] = $(this).val();
-			};
-			$('#save').removeClass('btn-success').addClass('btn-danger').text('delete ID setting');
+    // localStorageをセット
+    if(flag){ 
+      var ls_edit = function(){
+        localStorage[$(this).attr('id')] = $(this).val();
+      };
+      $('#save').removeClass('btn-success').addClass('btn-danger').text('delete ID setting');
 
-		// localStorageを削除
-		}else{ 
-			var ls_edit = function(){
-				localStorage.removeItem($(this).attr('id'));
-				$.removeCookie($(this).attr('id'));
-				$(this).val('');
-			};
-			$('#save').removeClass('btn-danger').addClass('btn-success').text('save ID setting');
-		}
+    // localStorageを削除
+    }else{ 
+      var ls_edit = function(){
+        localStorage.removeItem($(this).attr('id'));
+        $.removeCookie($(this).attr('id'));
+        $(this).val('');
+      };
+      $('#save').removeClass('btn-danger').addClass('btn-success').text('save ID setting');
+    }
 
-		if(!unedit) $('#first .item :text').filter(notEmpty).each(ls_edit);
-	}
+    if(!unedit) $('#first .item :text').filter(notEmpty).each(ls_edit);
+  }
 
-	$('#save').click(function(){
-		ls_btn_toggle($(this).hasClass('btn-success'));
-	});	
+  $('#save').click(function(){
+    ls_btn_toggle($(this).hasClass('btn-success'));
+  }); 
 
-	// localStorageかcookieがあれば入力して、ボタンを削除に変更
-	var ls_flag = false;
-	$('#first .item :text').each(function() {
-		var id = localStorage[$(this).attr('id')] || $.cookie($(this).attr('id'));
-		if(id){
-			$(this).val(id);
-			ls_flag = true;
-		}
-		$(this).on('keydown', function(){
-			if($(this).val() !== localStorage[$(this).attr('id')]){
-				ls_btn_toggle(false, true);
-			}
-		})
-	});
-	ls_btn_toggle(ls_flag);
+  // localStorageかcookieがあれば入力して、ボタンを削除に変更
+  var ls_flag = false;
+  $('#first .item :text').each(function() {
+    var id = localStorage[$(this).attr('id')] || $.cookie($(this).attr('id'));
+    if(id){
+      $(this).val(id);
+      ls_flag = true;
+    }
+    $(this).on('keydown', function(){
+      if($(this).val() !== localStorage[$(this).attr('id')]){
+        ls_btn_toggle(false, true);
+      }
+    })
+  });
+  ls_btn_toggle(ls_flag);
 
-	// localStorageがなければ入力欄を表示
-	if(!ls_flag) $('#tab').click();
-		
+  // localStorageがなければ入力欄を表示
+  if(!ls_flag) $('#tab').click();
+    
 });
 
 // modal用の日付フォーマット
 var popup_date = function(date){
-	if(typeof date !== 'object') date = new Date(date);
-	var y = date.getFullYear();
-	var m = date.getMonth() + 1;
-	var d = date.getDate();
-	var w = date.getDay();
-	var h = date.getHours();
-	var i = date.getMinutes();
-	var week = ['日', '月', '火', '水', '木', '金', '土'];
-	return y+'年'+m+'月'+d+'日（'+week[w]+'）'+h+':'+(('0'+i).slice(-2));
+  if(typeof date !== 'object') date = new Date(date);
+  var y = date.getFullYear();
+  var m = date.getMonth() + 1;
+  var d = date.getDate();
+  var w = date.getDay();
+  var h = date.getHours();
+  var i = date.getMinutes();
+  var week = ['日', '月', '火', '水', '木', '金', '土'];
+  return y+'年'+m+'月'+d+'日（'+week[w]+'）'+h+':'+(('0'+i).slice(-2));
 };
